c4a7fa239128c6cc7bd7d92f4cf33e2e71b309de
Julianalopesco <julianalopesmf@gmail.com>
2023-04-13 14:56:28 -0300

    ajuste controller

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
+//@Repository
 
 }
